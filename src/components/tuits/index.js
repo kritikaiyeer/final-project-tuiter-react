a04de99e6ac20ccdbb203eeb7df6da423ff8ae60
Darshi Shah <darshi24.shah@gmail.com>
2022-03-30 18:25:32 -0400

    working on getting my tuits screen

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -1,21 +1,25 @@
 import React from "react";
 import './tuits.css';
 import Tuit from "./tuit";
+import * as service from "../../services/tuits-service";
+const Tuits = ({tuits = [], refreshTuits}) => {
 
-export function Tuits({tuits = [], deleteTuit}) {
+    const deleteTuit = (tid) =>
+        service.deleteTuit(tid)
+            .then(refreshTuits);
+
     return (
-    <div>
-      <ul className="ttr-tuits list-group">
-        {
-          tuits.map && tuits.map(tuit => {
-            return (
-              <Tuit key={tuit._id} deleteTuit={deleteTuit} tuit={tuit}/>
-            );
-          })
-        }
-      </ul>
-    </div>
-  );
+        <div>
+          <ul className="ttr-tuits list-group">
+            {
+              tuits.map && tuits.map(tuit =>
+                  <Tuit key={tuit._id}
+                        deleteTuit={deleteTuit}
+                        tuit={tuit}/>)
+            }
+          </ul>
+        </div>
+      );
 }
 
 export default Tuits;
