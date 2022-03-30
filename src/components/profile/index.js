c04145a730846aaf53209e498e5c7dda0d12d4c6
Darshi Shah <darshi24.shah@gmail.com>
2022-03-29 21:23:48 -0400

    getting tuits

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -11,6 +11,7 @@ const Profile = () => {
   useEffect(async () => {
     try {
       const user = await service.profile();
+      console.log(user)
       setProfile(user);
     } catch (e) {
       navigate('/login');
