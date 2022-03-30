7aeb28b3543ae8629e9e60f220f0c283cc1cf025
Darshi Shah <darshi24.shah@gmail.com>
2022-03-30 18:40:07 -0400

    working on getting my tuits screen

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,28 +1,25 @@
 import React from "react";
 
-export default class TuitStats extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
+const TuitStats = ({tuit}) => {
     return (
       <div className="row mt-2">
         <div className="col">
           <i className="far fa-message me-1"></i>
-          {this.props.tuit.stats && this.props.tuit.stats.replies}
+          {tuit.stats && tuit.stats.replies}
         </div>
         <div className="col">
           <i className="far fa-retweet me-1"></i>
-          {this.props.tuit.stats && this.props.tuit.stats.retuits}
+          {tuit.stats && tuit.stats.retuits}
         </div>
         <div className="col">
           <i className="far fa-heart me-1"></i>
-          {this.props.tuit.stats && this.props.tuit.stats.likes}
+            {tuit.stats && tuit.stats.likes}
+
         </div>
         <div className="col">
           <i className="far fa-inbox-out"></i>
         </div>
       </div>
     );
-  }
 }
+export default TuitStats;
