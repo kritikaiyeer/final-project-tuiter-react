6cc98077918ce504f6ef80b2699969c71f7fdfec
Darshi Shah <darshi24.shah@gmail.com>
2022-03-30 18:55:53 -0400

    working on getting my tuits screen

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -2,10 +2,10 @@ import React, {useEffect, useState} from "react";
 import Tuits from "../tuits";
 import {HashRouter, Link, Route, Routes, useNavigate, useLocation} from "react-router-dom";
 import * as service from "../../services/auth-service";
-import MyTuits from "./my-tuits";
-import TuitsAndReplies from "./tuits-and-replies";
-import Media from "./media";
-
+import MyTuits from "../my-tuits";
+import TuitsAndReplies from "../tuits-and-replies";
+import Media from "../media";
+import MyLikes from "../my-likes";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -95,7 +95,7 @@ const Profile = () => {
         <Route path="/mytuits" element={<MyTuits/>}/>
         <Route path="/tuits-and-replies" element={<TuitsAndReplies/>}/>
         <Route path="/media" element={<Media/>}/>
-
+        <Route path="/likes" element={<MyLikes/>}/>
       </Routes>
     </div>
   );
---
+++ src/components/profile/my-likes.js
@@ -0,0 +1,8 @@
+const MyLikes = () => {
+    return(
+        <div>
+            <h1>Liked Tuits</h1>
+        </div>
+    );
+};
+export default MyLikes;
