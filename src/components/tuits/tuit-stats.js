3c6c04757f171d6adf51021bc66276d071d7a0ca
Darshi Shah <darshi24.shah@gmail.com>
2022-03-31 22:02:44 -0400

    fixing likes

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TuitStats = ({tuit, likeTuit = () => {}}) => {
+const TuitStats = ({tuit, likeTuit}) => {
     return (
       <div className="row mt-2">
         <div className="col">
@@ -21,7 +21,7 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
                 tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
                   <i className="far fa-heart me-1"></i>
               }
-            <span className="ttr-stats-likes">number</span>
+            <span className="ttr-stats-likes">{tuit.stats && tuit.stats.likes}</span>
           </span>
         </div>
         <div className="col">
