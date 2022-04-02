5934c617630445d7c4c01488f72b44f7fe886d83
Darshi Shah <darshi24.shah@gmail.com>
2022-04-01 22:33:15 -0400

    adding likes-screen feature

--- src/components/profile/my-likes.js
+++ src/components/profile/my-likes.js
@@ -1,7 +1,18 @@
+import Tuits from "../tuits";
+import * as service from "../../services/likes-service";
+import {useEffect, useState} from "react";
+
 const MyLikes = () => {
+    const [likedTuits, setLikedTuits] = useState([]);
+    const findTuitsILike = () =>
+        service.findAllTuitsLikedByUser("me")
+            .then((tuits) => setLikedTuits(tuits));
+    useEffect(findTuitsILike, []);
+
     return(
         <div>
-            <h1>Liked Tuits</h1>
+            <h2>My Likes</h2>
+            <Tuits tuits={likedTuits} refreshTuits={findTuitsILike}/>
         </div>
     );
 };
